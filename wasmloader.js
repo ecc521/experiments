f39f0653cb90ec6844c9b95e78ec815961aee582
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-10-24 21:23:21 -0400

    Update wasmloader.js

--- wasmloader.js
+++ wasmloader.js
@@ -1,12 +1,17 @@
+//Pass in a WebAssembly instance as the first parameter.
+//This will create (or add to) a global WebAssembly object containing all the functions exported by the instance.
+//This function automatically removes C++ debug markers. If you do not want it to, pass in true as the second parameter.
+
 function loadexports(instance, keepdebugmarkers) {
     if (!window.wasm) {
     window.wasm = {}
     } 
     for (let key in instance.exports) {
         let item = instance.exports[key]
+        //We don't want any non-functions like the memory object
         if (typeof item === "function") {
             let name = key
-            //Check for C++ Debug Markers, and Remove Them
+            //Check for C++ Debug Markers, and Remove Them if Needed
             if (key.slice(0,3) === "_Z7" && key.slice(-1) === "i" && !keepdebugmarkers) {
                 name = key.slice(3, key.length-1)  
             }
