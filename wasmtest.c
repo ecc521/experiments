e57c39054c96608be797be17b568c79fa275da35
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-10-27 17:11:08 -0400

    Update wasmtest.c

--- wasmtest.c
+++ wasmtest.c
@@ -1,9 +1,12 @@
 unsigned char allocate(unsigned long len) {
   unsigned char * mem = malloc(len);
-  
+  return &mem;
+}
+
+
+unsigned char set(unsigned char * ptr, unsigned long len) {
   for (unsigned long i=0;i<len;i++) {
-    mem[i] = i%256;
+    *(ptr+i) = i%256;
   }
-  
-  return &mem[0];
+  return ptr;
 }
