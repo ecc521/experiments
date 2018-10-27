4656d52bb4018ecefe7641adc7b9df376c37928d
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-10-26 21:22:05 -0400

    Create wasmtest.js

---
+++ wasmtest.js
@@ -0,0 +1,24 @@
+
+
+var wasmModule = new WebAssembly.Module(wasmCode);
+var wasmInstance = new WebAssembly.Instance(wasmModule, wasmImports);
+//log(wasmInstance.exports.getChar(0));
+
+wasmInstance.exports.memory = new WebAssembly.Memory({initial:20});
+
+const memory = wasmInstance.exports.memory;
+//const strBuf = new Uint8Array(memory.buffer, wasmInstance.exports.getStrOffset());
+	
+	
+
+wasmInstance.exports.memory.grow(9)
+log(wasmInstance.exports.memory.buffer.byteLength)
+	
+	
+console.log(memory)
+	
+var buflen = 100000
+	
+const strBuf = new Uint8Array(memory.buffer, wasmInstance.exports.getbuf(buflen), buflen);
+
+log(strBuf)
