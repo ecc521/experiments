ed5d9d6fa0eb34ecb523b3d73443170a29553726
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-10-31 19:08:39 -0400

    Update sw.js

--- notifications/sw.js
+++ notifications/sw.js
@@ -1,6 +1,6 @@
 async function handlefetch(event) {
   console.log(event)
-  return fetch(event.request)
+  event.respondWith(fetch(event.request))
 }
 
 self.addEventListener("fetch", handlefetch) 
