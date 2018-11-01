7128cda79f99efe0c4c9de7634907f0932d9fad6
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-11-01 15:14:30 -0400

    Update sw.js

--- notifications/sw.js
+++ notifications/sw.js
@@ -5,15 +5,16 @@ async function handlefetch(event) {
   console.log(url)
   
   //Network, fall back to cache
-  let response;
+  let response, cache;
   response = fetch(event.request)
   
+  cache = await caches.open("rivers.run")
+
   response.catch(function(event){
       //USGS data is date dependent. We should therefore ignore the query parameter
       response = cache.match(request, {ignoreSearch:true})
   })
 
-  let cache = caches.open("rivers.run")
   cache.put(url, response)
 
   let keys = await cache.keys()
